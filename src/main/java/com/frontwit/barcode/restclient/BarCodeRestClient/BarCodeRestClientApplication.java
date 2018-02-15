793fef6ba29be03c18facc218d9d45feb1b7bd9d
Patryk Witiw <witiw.p@gmail.com>
2018-02-15 15:23:55 +0100

    init commit

---
+++ src/main/java/com/frontwit/barcode/restclient/BarCodeRestClient/BarCodeRestClientApplication.java
@@ -0,0 +1,12 @@
+package com.frontwit.barcode.restclient.BarCodeRestClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarCodeRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarCodeRestClientApplication.class, args);
+	}
+}
